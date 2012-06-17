cb908cf03465c792a1da20402b2255415a04bd41
gaedej <ianetman@gmail.com>
2012-06-17 11:00:48 -0500

    Appended 4 integers to the front of x and y

--- workingSequence.R
+++ workingSequence.R
@@ -1,11 +1,11 @@
 # Global Comments More
-x <- c(50:1)
-y <- c(1:50)
+x <- c(8:10,c(50:1))
+y <- c(7,4,8,c(1:50))
 z <- y[1]:y[3]
 print(z)
-for(i in 1:x){
-  print(x[i]:x[i+2])
-  q <- c(x[i]:x[i+2])
-  print(z)
-  print(cor(z,q))
-}
+# for(i in 1:x){
+#   print(x[i]:x[i+2])
+#   q <- c(x[i]:x[i+2])
+#   print(z)
+#   print(cor(z,q))
+# }
