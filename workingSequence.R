dfcec2be8a358a487a28fcf929aed992b18e17da
gaedej <ianetman@gmail.com>
2012-06-17 19:50:22 -0500

    Got the increment of x working so correlation is operational

--- workingSequence.R
+++ workingSequence.R
@@ -1,11 +1,22 @@
 # Global Comments More
-x <- c(8:10,c(50:1))
-y <- c(7,4,8,c(1:50))
-z <- y[1]:y[3]
+x <- c(8:15,c(50:1))
+y <- c(7,4,8,4,1,11,c(1:50))
+z <- c(y[1],y[2],y[3])
 print(z)
-# for(i in 1:x){
-#   print(x[i]:x[i+2])
-#   q <- c(x[i]:x[i+2])
+print("outside Loop")
+counter = 1
+
+for(i in x){
+  print("###########")
+  print(counter)
+  # print(x[i]:x[i+2])
+  q <- c(x[counter], x[counter+1], x[counter+2])
+#   print(i)
+#   print(q)
 #   print(z)
-#   print(cor(z,q))
-# }
+  print(cor(z,q))
+  print("###########")
+#   goo <- cor(z,q)
+#   print(goo)
+  counter <- counter+1
+}
