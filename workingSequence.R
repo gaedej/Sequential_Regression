1950e2e157618f4562b1c9ab12f5753646ebd469
gaedej <ianetman@gmail.com>
2012-06-17 20:19:01 -0500

    Added Nested Loop

--- workingSequence.R
+++ workingSequence.R
@@ -2,21 +2,23 @@
 x <- c(8:15,c(50:1))
 y <- c(7,4,8,4,1,11,c(1:50))
 z <- c(y[1],y[2],y[3])
-print(z)
-print("outside Loop")
-counter = 1
+# print(z)
 
-for(i in x){
-  print("###########")
-  print(counter)
-  # print(x[i]:x[i+2])
-  q <- c(x[counter], x[counter+1], x[counter+2])
-#   print(i)
-#   print(q)
-#   print(z)
-  print(cor(z,q))
-  print("###########")
-#   goo <- cor(z,q)
-#   print(goo)
-  counter <- counter+1
+counter2 = 1
+for(i in y){
+  p <- c(y[counter2], y[counter2+1], y[counter2+2])
+#   print(p)
+  counter = 1
+  for(i in x){
+    # print("###########")
+    # print(counter)
+    q <- c(x[counter], x[counter+1], x[counter+2])
+    print(p)
+    print(q)
+    print(cor(p,q))
+    # print("###########")
+    counter <- counter+1
+  }
+  counter2 <- counter2+1
+  # print(counter2)
 }
