9c5fe949e3704b313be103dead7e2eb54dee1a83
gaedej <ianetman@gmail.com>
2012-06-21 16:19:02 -0500

    Cleaned up code, shortened vector

--- workingSequence_V2.R
+++ workingSequence_V2.R
@@ -1,18 +1,33 @@
 # Global Comments More
 # Set Variables
-x <- c(8:15,c(50:1))
-y <- c(7,4,8,4,1,11,4,9,c(1:50))
+x <- c(8:15,c(5:1))
+y <- c(7,4,8,4,1,11,4,9,c(1:5))
 z <- c(y[1],y[2],y[3])
+boo <- c(1:length(x))
+hoo <- c(1:length(x))
+
 counter = 1
 counter2 = 1
+
 my_correlation = NULL
 
 # Start Nested Loops
-for(i in boo){
-  cat(i,"   " )
+for(i1 in boo){
+  p <- c(x[i1], x[i1+1], x[i1+2])
+  # cat(p)
+  cat("   " )
    for(i2 in hoo){ 
-     cat(i2,"   ")
+     q <- c(y[i2], y[i2+1], y[i2+2])
+#      cat("*")
+#      cat(p)
+#      cat("*")
+#      cat(q)
+#      cat("*")
+     boom <- cor(p,q)
+     cat(boom)
+     cat("   ")
   }
    cat("**********")
   }
+plot(boom, type = "l")
   
