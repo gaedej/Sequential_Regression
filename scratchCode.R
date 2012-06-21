488fa5eb19481e8274340e63700fd630969fbdc2
gaedej <ianetman@gmail.com>
2012-06-20 23:43:08 -0500

    Fixing Nested Loop Still work to do

---
+++ scratchCode.R
@@ -0,0 +1,8 @@
+boo <- c(1:10)
+hoo <- c(1:3)
+for(i in boo){
+  print(i)
+  for(x in hoo){
+    print(hoo)
+  }
+}
--- workingSequence.R
+++ workingSequence.R
@@ -2,28 +2,33 @@
 x <- c(8:15,c(50:1))
 y <- c(7,4,8,4,1,11,c(1:50))
 z <- c(y[1],y[2],y[3])
-# print(z)
 
+counter = 1
 counter2 = 1
-for(i in y){
+my_correlation = NULL
+for(i in length(y)){
   p <- c(y[counter2], y[counter2+1], y[counter2+2])
-#   print(p)
-  counter = 1
-  for(i in x){
+  if(counter < length(y)-3){
+    
+   for(i in x){
     if(counter < length(x)-3){
-    # print("###########")
-    # print(counter)
     q <- c(x[counter], x[counter+1], x[counter+2])
-    print(p)
-    print(q)
+#     print(p)
+#     print(q)
     cor_out <- cor(p,q)
     if(cor_out)
-    print(cor_out)
-
-    # print("###########")
-  }
+    # print(cor_out)
+    my_correlation <- append(my_correlation, cor_out)
+    }
     counter <- counter+1
+    print(counter)
+  }
   }
   counter2 <- counter2+1
-  # print(counter2)
+  print(counter2)
+  
 }
+plot(my_correlation, type="l", col="green")
+
+print(my_correlation)
+
