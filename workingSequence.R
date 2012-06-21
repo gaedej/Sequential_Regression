4f5d1340ec343dd2ae7ef1d17e8fb1b98fa0e8ae
gaedej <ianetman@gmail.com>
2012-06-20 23:55:54 -0500

    initiated hoo

--- workingSequence.R
+++ workingSequence.R
@@ -3,6 +3,7 @@ x <- c(8:15,c(50:1))
 y <- c(7,4,8,4,1,11,c(1:50))
 z <- c(y[1],y[2],y[3])
 boo <- c(1:53)
+hoo <- c(1:53)
 counter = 1
 counter2 = 1
 my_correlation = NULL
@@ -10,7 +11,7 @@ for(i in boo){
   p <- c(y[counter2], y[counter2+1], y[counter2+2])
   if(counter < length(y)-3){
     
-   for(i2 in x){
+   for(i in hoo){
     if(counter < length(x)-3){
     q <- c(x[counter], x[counter+1], x[counter+2])
 #     print(p)
