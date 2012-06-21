231d1e03fdb04a367d451ed3b3dad7a460317e80
gaedej <ianetman@gmail.com>
2012-06-21 11:04:58 -0500

    Started rewrite

--- workingSequence.R
+++ workingSequence.R
@@ -1,17 +1,18 @@
 # Global Comments More
 x <- c(8:15,c(50:1))
-y <- c(7,4,8,4,1,11,c(1:50))
+y <- c(7,4,8,4,1,11,4,9,c(1:50))
 z <- c(y[1],y[2],y[3])
-boo <- c(1:53)
-hoo <- c(1:53)
 counter = 1
 counter2 = 1
 my_correlation = NULL
 for(i in boo){
   p <- c(y[counter2], y[counter2+1], y[counter2+2])
+  cat(p,"   "  )
+  cat(i,"   " )
   if(counter < length(y)-3){
     
    for(i in hoo){
+     print("tick")
     if(counter < length(x)-3){
     q <- c(x[counter], x[counter+1], x[counter+2])
 #     print(p)
@@ -20,10 +21,13 @@ for(i in boo){
     if(cor_out)
     # print(cor_out)
     my_correlation <- append(my_correlation, cor_out)
+    print(i)
     }
     counter <- counter+1
     print(counter)
+    
   }
+   
   }
   counter2 <- counter2+1
   print(counter2)
@@ -32,5 +36,5 @@ for(i in boo){
 }
 plot(my_correlation, type="l", col="green")
 
-print(my_correlation)
+# print(my_correlation)
 
---
+++ workingSequence_V2.R
@@ -0,0 +1,18 @@
+# Global Comments More
+# Set Variables
+x <- c(8:15,c(50:1))
+y <- c(7,4,8,4,1,11,4,9,c(1:50))
+z <- c(y[1],y[2],y[3])
+counter = 1
+counter2 = 1
+my_correlation = NULL
+
+# Start Nested Loops
+for(i in boo){
+  cat(i,"   " )
+   for(i2 in hoo){ 
+     cat(i2,"   ")
+  }
+   cat("**********")
+  }
+  
