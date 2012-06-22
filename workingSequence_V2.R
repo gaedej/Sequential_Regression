5ed4f4e65683f2e52ed4bc94f914b9f5e6ab1bb7
gaedej <ianetman@gmail.com>
2012-06-21 19:23:09 -0500

    Its working as planned.  Now on to the slope.

--- workingSequence_V2.R
+++ workingSequence_V2.R
@@ -5,18 +5,19 @@ y <- c(7,4,8,4,1,11,4,9,c(1:5))
 z <- c(y[1],y[2],y[3])
 boo <- c(1:length(x))
 hoo <- c(1:length(x))
-
+# set the length of the correlation vector
+corVecLeng = 3
 counter = 1
 counter2 = 1
 
 my_correlation = NULL
 
 # Start Nested Loops
-for(i1 in boo){
+for(i1 in head(boo, -corVecLeng)){
   p <- c(x[i1], x[i1+1], x[i1+2])
   # cat(p)
   cat("   " )
-   for(i2 in hoo){ 
+   for(i2 in head(hoo, -corVecLeng)){ 
      q <- c(y[i2], y[i2+1], y[i2+2])
 #      cat("*")
 #      cat(p)
@@ -27,7 +28,7 @@ for(i1 in boo){
      cat(boom)
      cat("   ")
   }
-   cat("**********")
+   # cat("**********")
   }
-plot(boom, type = "l")
+# plot(boom, type = "l")
   
