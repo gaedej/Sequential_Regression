6204b43ebd6894ecf9b8aa2bb3502859e7475721
gaedej <ianetman@gmail.com>
2012-07-18 22:22:18 -0500

    Did Garbage Collection.  Still runs clean out some unneeded variables next.

--- PracticeGGPlot.R
+++ PracticeGGPlot.R
@@ -11,16 +11,9 @@ CorThreshold <- Sequence[2,1]
 ########### Next Line Starts HTML Save Function #####
 saveHTML(
 for(i in c(1:nrow(Correlation_DataFrame))){
-# plot(x, type="b", ylim=c(20,100))
-# points(y)
-# lines(y)
-# rect(20,85,30,100)
-# text(24, 98, "Correlation Value" )
-# text(24, 96, Correlation_DataFrame[i,"CorValue"] )
 #################### This Section populates took and pook
 xAxisA <- Correlation_DataFrame[,"Xnum1"]
 xAxisB <- Correlation_DataFrame[,"Xnum2"]
-
 ######################## This Where I am working ############
 y1b1 <- Correlation_DataFrame[i,"Y2"]
 y1b2 <- Correlation_DataFrame[i,"Y2A"]
@@ -39,63 +32,23 @@ tempL1 <- NULL
 LIterations <- (c(1:Iterations))+2
 y1 <- Correlation_DataFrame[i,"Y1"]
 Nr <- nrow(Correlation_DataFrame)
-
-# while(LIt <= Nr){
-  for(i in LIterations){
+for(i in LIterations){
   tempL1 <- paste("y1",alphaSeries[i],sep="")
   tempL1 <- c(Correlation_DataFrame[LIt,i]) # removed i+1
   took <- c(took, tempL1)
-  
 }
-  took <- c(took, tempL1)
-print(took)
-
-# }
+took <- c(took, tempL1)
+# print(took)
 pook <- NULL
 yv1a <- Correlation_DataFrame[i,"Y1A"]
 for(i in c((length(Correlation_DataFrame)-(length(LIterations)+1)):(length(Correlation_DataFrame)-1))){
   tempL2 <- paste("y2",alphaSeries[i],Nr,sep="")
   tempL2 <- c(Correlation_DataFrame[LIt,i]) # removed i+1
   pook <- c(pook, tempL2)
-  # pook <- c(pook, paste("yv1",alphaSeries[i],i,sep=""))
 }
-print(pook)
+# print(pook)
 LIt = LIt + 1
-# took <- c(y1,y1b1,y1c1,y1d1,y1e1)
-# pook <- c(yv1a,yv1b2,yv1c2,yv1d2,yv1e2)
 kook <- lm(took ~ pook)
-# abline(kook)
-#### P0
-# pulll <- c(bb,cc)
-# pushh <- c(y1,yv1a)
-# #### P1
-# pulll4 <- c(bb+1,cc+1)
-# pushh4 <- c(y1b1,y1b2)
-# #### P2
-# pulll5 <- c(bb+2,cc+2)
-# pushh5 <- c(y1c1,y1c2)
-# #### P3
-# pulll6 <- c(bb+3,cc+3)
-# pushh6 <- c(y1d1,y1d2)
-# #### P4
-# pulll7 <- c(bb+4,cc+4)
-# pushh7 <- c(y1e1,y1e2)
-# Lines are probably redundant. Commenting out for the time being.
-# lines(pulll,pushh, col="red")
-# lines(pulll4, pushh4, col="red")
-# lines(pulll5, pushh5, col="red")
-# lines(pulll6, pushh6, col="red")
-# lines(pulll7, pushh7, col="red")
-# title(main = "Leading Indicators for DE and LNT")
-# polygon(c(pulll,rev(pulll4)), c(pushh,rev(pushh4)), col="orange", border="red")
-# polygon(c(pulll4,rev(pulll5)), c(pushh4,rev(pushh5)), col="blue", border="red")
-# polygon(c(pulll5,rev(pulll6)), c(pushh5,rev(pushh6)), col="orange", border="red")
-# polygon(c(pulll6,rev(pulll7)), c(pushh6,rev(pushh7)), col="blue", border="red")
-# }
-#  , img.name = "Correlation_Sequence", ani.height = 600, ani.width = 1000)
-########### Previous Line Ends HTML Save Function ####
-#################################
-#################################
 ###############  New ggplot2 section #####
 vp <- viewport(width = 0.4, height = 0.4, x = 1,
     y = unit(0.7, "lines"), just = c("right","bottom"))
@@ -103,11 +56,9 @@ vp <- viewport(width = 0.4, height = 0.4, x = 1,
 mainp <-
 ggplot() + 
 layer(
-#     data = Correlation_DataFrame, mapping = aes(x = c(1:54), y = x),
       mapping = aes(x = c(1:54), y = x),
       geom = "line", stat = "identity" , color = "blue") +
 layer(
-    # data = Correlation_DataFrame, mapping = aes(x = c(1:54), y = y),
     mapping = aes(x = c(1:54), y = y),
     geom = "line", stat = "identity", col = "red")  +
 geom_line(aes(x=pulll,y=pushh)) + 
@@ -118,17 +69,10 @@ geom_line(aes(x=pulll7,y=pushh7))# +
 geom_line(aes(x=pulll8,y=pushh8))
 
 ########## Main Focus Data Prep.
-# xDistiledx <- c(pulll[1], pulll4[1], pulll5[1], pulll6[1], pulll7[1])
-# xDistiledy <- c(pulll[2], pulll4[2], pulll5[2], pulll6[2],pulll7[2])
-# xDistiledx <- c(pushh[1], pushh4[1], pushh5[1], pushh6[1], pushh7[1])
-# xDistiledy <- c(pushh[2], pushh4[2], pushh5[2], pushh6[2],pushh7[2])
 xDistiledx <- took
 xDistiledy <- pook
 
-# xDistiledxScat <- c(1,pushh[1], 2,  pushh4[1], 3 , pushh5[1],4 , pushh6[1], 5 ,pushh7[1])
-# xDistiledyScat <- c(1, pushh[2], 2,  pushh4[2], 3, pushh5[2],4,  pushh6[2], 5, pushh7[2])
 DistCombo <- as.data.frame(c(xDistiledx, xDistiledy))
-# colnames(DistCombo) <- c("scatX", "scatY")
 DistCombo1 = as.data.frame(matrix(nrow = 5, ncol = 2))
 DistCombo1[1] <- as.data.frame(c(1:length(pook)))
 DistCombo1[2] <- as.data.frame(xDistiledx)
@@ -138,9 +82,6 @@ DistCombo2[2] <- as.data.frame(xDistiledy)
 DistCombo3 <- rbind(DistCombo1,DistCombo2)
 c <- ggplot(DistCombo3 , aes(V1, V2))
 yDistiled <- c(pushh, pushh4, pushh5, pushh6, pushh7)
-# xDistiled <- c(1:4)
-# yDistiled <- c(5:2)
-############## Snippet from Book ############  c + stat_smooth(method = "lm") + geom_point()
 mainFocus <- ggplot() +
   layer(
     mapping = aes(x = c(1:len(took)) , y = xDistiledx), geom = "line", stat = "identity" , color = "blue"
@@ -174,15 +115,12 @@ theme_set(theme_bw())
 theme_white()
 
 full_combo <- function() {
-  # print(mainp)
-  # print(mainFocus)
   print(Regres)
   theme_set(theme_bw(base_size = 8))
   theme_white()
   print(mainp, vp = vp)
   theme_set(theme_bw())
 }
-
 full_combo()
 }
 , img.name = "Correlation_Sequence", ani.height = 600, ani.width = 1000)
